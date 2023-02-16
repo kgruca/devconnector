59e524ce47db968d1b1ff2410e8ab976dd768b67
Krzysztof Gruca <kgruca21@gmail.com>
2023-02-15 23:07:46 -0800

    delete a comment from a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const {check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 const Post = require('../../models/Post');
+const { findById } = require('../../models/User');
 
 // @route   POST api/posts
 // @desc    Create a post
@@ -198,5 +199,41 @@ router.post('/comment/:id',
         }
     }
 );
+
+// @route   DELETE api/posts/comment/:id
+// @desc    Delete comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Make sure comment exists
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment does not exist' });
+        }
+
+        // Check user
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json('User not authorized');
+        }
+
+        // Get remove index 
+        const removeIndex = post.comments
+            .map(comment => comment.user.toString())
+            .indexOf(req.user.id);
+
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
