fb8bc91b03a43997a8b4784622fb0a4e8faf6af0
Krzysztof Gruca <kgruca21@gmail.com>
2023-03-02 20:14:47 -0800

    created redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,23 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-    <Router>
-        <Navbar />
-        <Routes>
-            <Route exact path="/" element={<Landing />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/login" element={<Login />} />
-        </Routes>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Landing />} />
+                <Route exact path="/register" element={<Register />} />
+                <Route exact path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    </Provider>
 );
 
 export default App;
